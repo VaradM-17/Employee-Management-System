e670a77d2b38650066d4f81851594cb2cc4e329e
Varad Mule <varadmule17@gmail.com>
2025-03-28 22:48:03 +0530

    Folders created

---
+++ EMS-Frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmloyeeDetailsComponent } from './emloyee-details/emloyee-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmloyeeDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
